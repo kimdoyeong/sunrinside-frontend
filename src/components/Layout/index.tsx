a253db8f13968ffc796eff3c8e74d9f481201368
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-05-21 22:16:42 +0900

    Update font-family

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -26,8 +26,7 @@ function Layout() {
 const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
-
-        
+        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
     
     /* Dark Mode*/
