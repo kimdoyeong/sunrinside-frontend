0ee68037852bdb911131678a8d1a104583fbd92e
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-05-22 17:00:52 +0900

    Apply react-modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import Modal from "react-modal";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store";
 
+const root = document.getElementById("root");
+
+root && Modal.setAppElement(root);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  root
 );
 
 // If you want your app to work offline and load faster, you can change
