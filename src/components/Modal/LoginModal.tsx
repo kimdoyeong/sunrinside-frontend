953e984f6efd548dd6e3e7d160e02083f891b9f4
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-06 15:26:18 +0900

    Update LoginModal UI

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import Modal from "../UI/Modal";
+import Input, { WrapInput } from "../UI/Input";
+import Button from "../UI/Button";
 function LoginModal() {
   return (
     <Modal modalName="login">
       <h1>로그인</h1>
+
+      <form style={{ width: 300 }}>
+        <WrapInput fieldName="아이디">
+          <Input type="text" />
+        </WrapInput>
+        <WrapInput fieldName="비밀번호">
+          <Input type="password" />
+        </WrapInput>
+        <Button full>로그인</Button>
+      </form>
     </Modal>
   );
 }
