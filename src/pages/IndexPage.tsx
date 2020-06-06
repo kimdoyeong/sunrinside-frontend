3524815e89ee6923c6a5dae4d0dfba09abef3a08
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-06 22:12:05 +0900

    Update Index page

---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function IndexPage() {
+  return <Layout></Layout>;
+}
+export default IndexPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUpPage from "./SignUp";
+import IndexPage from "./IndexPage";
 
 function Routes() {
   return (
     <Switch>
+      <Route path="/" exact component={IndexPage} />
       <Route path="/signup" exact component={SignUpPage} />
     </Switch>
   );
