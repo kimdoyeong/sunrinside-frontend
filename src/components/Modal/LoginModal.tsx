d4ccb4db9b7c17c4ba8470f8e7baa3aacfc18ab2
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-06 22:31:52 +0900

    Update loginModal form control

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Modal from "../UI/Modal";
 import Input, { WrapInput } from "../UI/Input";
 import Button from "../UI/Button";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/reducers";
+import { toast } from "react-toastify";
+import { Login } from "../../store/slices/auth";
 function LoginModal() {
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+  const { error, pending } = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    error && toast.error(error);
+  }, [error]);
+
+  function onsubmit(data: any) {
+    dispatch(Login(data));
+  }
   return (
     <Modal modalName="login">
       <h1>로그인</h1>
 
-      <form style={{ width: 300 }}>
+      <form style={{ width: 300 }} onSubmit={handleSubmit(onsubmit)}>
         <WrapInput fieldName="아이디">
-          <Input type="text" />
+          <Input
+            type="text"
+            name="username"
+            ref={register({ required: true })}
+          />
         </WrapInput>
         <WrapInput fieldName="비밀번호">
-          <Input type="password" />
+          <Input
+            type="password"
+            name="password"
+            ref={register({ required: true })}
+          />
         </WrapInput>
-        <Button full>로그인</Button>
+        <Button full disabled={pending}>
+          {!pending ? "로그인" : "처리 중"}
+        </Button>
         <hr />
         <Link to="/signup">회원가입</Link>
       </form>
