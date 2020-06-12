6a7171ba56a0e743840de53508bdba5868f36cbe
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-12 12:40:32 +0900

    Update ListPage

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -3,11 +3,23 @@ import styled from "styled-components";
 import Layout from "../components/Layout";
 import Heading from "../components/UI/Heading";
 import ThreadList from "../components/ThreadList";
+import Button from "../components/UI/Button";
+import { Link } from "react-router-dom";
 
 function ListPage() {
   return (
     <Layout>
       <Style>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Link to="/write">
+            <Button>
+              <span role="img" aria-label="pen">
+                🖋
+              </span>{" "}
+              글쓰기
+            </Button>
+          </Link>
+        </div>
         <Heading>지금 뜨고 있는 글</Heading>
         <ThreadList type="trending" />
       </Style>
